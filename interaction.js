7d18623d9713e24fc299267d3e4d78c1f2c2665b
Diego <dym2312@hotmail.com>
2018-01-30 17:55:47 +0100

    Premiers deux parties tp%

--- interaction.js
+++ interaction.js
@@ -1,14 +1,40 @@
 
 // La création d'un Dnd requière un canvas et un interacteur.
+var canvas = document.getElementById('myCanvas');
 // L'interacteur viendra dans un second temps donc ne vous en souciez pas au départ.
 function DnD(canvas, interactor) {
-	// Définir ici les attributs de la 'classe'
-
+  // Définir ici les attributs de la 'classe'
+  var self = this;
+  self.xBeginPosition = 0;
+  self.yBeginPosition = 0;
+  self.xFinalPosition = 0;
+  self.yFinalPosition = 0;
+  self.isPressed = false;
+  
 	// Developper les 3 fonctions gérant les événements
-
-	// Associer les fonctions précédentes aux évènements du canvas.
+   self.onPressed = function(evt){
+     isPressed = true;
+     console.log("isPressed: "+isPressed);
+   }
+   self.onMove = function(evt){
+    if (isPressed) {
+       self.xBeginPosition = self.xFinalPosition;
+       self.yBeginPosition = self.yBeginPosition;
+       self.xFinalPosition = getMousePosition(canvas,evt).x;
+       self.yFinalPosition = getMousePosition(canvas,evt).y;
+       console.log("Begin Positions X: " + self.xBeginPosition + "Y: "+self.yBeginPosition);
+       console.log("Final Positions X: " + self.xFinalPosition + "Y: "+self.yFinalPosition);
+    }
+   }
+   self.onReleased = function(evt){
+      isPressed = false;
+      console.log("isPressed: "+isPressed);
+   }
+  // Associer les fonctions précédentes aux évènements du canvas.
+  canvas.addEventListener('mousedown', self.onPressed, false);
+  canvas.addEventListener('mousemove',self.onMove, false);
+  canvas.addEventListener('mouseup', self.onReleased, false);
 };
-
 
 // Place le point de l'événement evt relativement à la position du canvas.
 function getMousePosition(canvas, evt) {
--- model.js
+++ model.js
@@ -1,3 +1,32 @@
 
 // Implémenter ici les 4 classes du modèle.
+class Form {
+
+    constructor(couleur, epaisseur) {
+        this.couleur = couleur;
+        this.epaisseur = epaisseur;
+    }
+
+
+
+}
+class Rectangle extends Form{
+
+    constructor(pointX, largeur, hauteur, couleur, epaisseur) {
+        super(couleur, epaisseur);
+        this.pointX = pointX;
+        this.largeur = largeur;
+        this.hauteur = hauteur;
+    }
+}
+class line extends Form{
+    constructor(pointX,pointY,couleur, epaisseur) {
+        super(couleur, epaisseur);
+        this.pointX = pointX;
+        this.pointY = pointY;
+    }
+}
+function Drawing () {
+   var forms =  [];
+}
 // N'oubliez pas l'héritage !
